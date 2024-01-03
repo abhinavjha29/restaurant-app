0d19fe0d72b037a22fa2be9e2460d8943afda299
abhinavjha29 <abhinav_29@outlook.com>
2024-01-03 14:48:17 +0530

    added summary

--- Restaurant App/src/App.jsx
+++ Restaurant App/src/App.jsx
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import Header from "./components/UI/Header";
+import Summary from "./components/UI/Summary";
 
 function App() {
   return (
-    <>
+    <div>
       <Header></Header>
-    </>
+      <Summary></Summary>
+    </div>
   );
 }
 
---
+++ Restaurant App/src/components/UI/Summary.jsx
@@ -0,0 +1,19 @@
+const Summary = () => {
+  return (
+    <div className="card w-50 mb-3 summary">
+      <div className="card-body ">
+        <h4 className="card-title">Delcious Food , served to you</h4>
+        <p className="card-text">
+          "Culinary Delights Await! Explore a delectable array of dishes and
+          savor a gastronomic journey at our restaurant
+        </p>
+        <p className="card-text">
+          From flavorful cuisines to impeccable service, indulge in a memorable
+          dining experience with us
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
